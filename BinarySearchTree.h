f795233f4abeea7283c557aa48ffa623ba1a3bf1
sai2327 <saitejak2327@gmail.com>
2025-07-18 00:21:26 +0530

    Update BinarySearchTree.h

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -26,55 +26,61 @@ struct BinarySearchTree {
         root = ADD(val, root);
     }
     // Inorder traversal
-    void PrintInorder(TreeNode *node) {
-        if (node == NULL) return;
+    void PrintInorder(TreeNode *node){
+        if (node==NULL) return;
         PrintInorder(node->left);
-        cout << node->data << " ";
+        cout<<node->data<<" ";
         PrintInorder(node->right);
     }
-    void Inorder() {
+    void Inorder(){
         PrintInorder(root);
-        cout << endl;
+        cout<<endl;
+    }
+    //to find the minimum value of tree
+    TreeNode* FindMin(TreeNode *node) {
+        while (node && node->left) node = node->left;
+        return node;
     }
     // Search
-    bool SEARCH(TreeNode *node, int key) {
-        if (node == NULL) return false;
-        if (key == node->data) return true;
-        if (key < node->data)
-            return SEARCH(node->left, key);
+    bool SEARCH(TreeNode *node,int key){
+        if (node==NULL) return false;
+        if (key==node->data) return true;
+        if (key<node->data)
+            return SEARCH(node->left,key);
         else
-            return SEARCH(node->right, key);
+            return SEARCH(node->right,key);
     }
     bool Search(int key) {
-        return SEARCH(root, key);
+        return SEARCH(root,key);
     }
     // Delete
     TreeNode* DELETE(TreeNode *node, int key) {
-        if (node == NULL) return NULL;
-        if (key < node->data)
-            node->left = DELETE(node->left, key);
-        else if (key > node->data)
-            node->right = DELETE(node->right, key);
+        if (node==NULL) return NULL;
+        if (key<node->data)
+            node->left=DELETE(node->left, key);
+        else if (key>node->data)
+            node->right=DELETE(node->right,key);
         else {
             // Node with one child or no child
-            if (node->left == NULL) {
-                TreeNode *temp = node->right;
+            if(node->left==NULL){
+                TreeNode *temp=node->right;
                 delete node;
                 return temp;
-            } else if (node->right == NULL) {
-                TreeNode *temp = node->left;
+            }
+            else if (node->right==NULL){
+                TreeNode *temp=node->left;
                 delete node;
                 return temp;
             }
             // Node with two children
-            TreeNode *temp = FindMin(node->right);
-            node->data = temp->data;
-            node->right = DELETE(node->right, temp->data);
+            TreeNode *temp=FindMin(node->right);
+            node->data=temp->data;
+            node->right=DELETE(node->right,temp->data);
         }
         return node;
     }
     void Delete(int key) {
-        root = DELETE(root, key);
+        root=DELETE(root, key);
     }
 };
 #endif
